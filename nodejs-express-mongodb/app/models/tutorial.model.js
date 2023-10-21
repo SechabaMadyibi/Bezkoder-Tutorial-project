8033722b6b56b948f3b01a8ac3ae680a3afb6fba
SechabaMadyibi <sechabamadyibi1@gmail.com>
2023-10-21 14:53:54 +0200

    21 October

--- nodejs-express-mongodb/app/models/tutorial.model.js
+++ nodejs-express-mongodb/app/models/tutorial.model.js
@@ -8,6 +8,9 @@ module.exports = mongoose => {
       { timestamps: true }
     );
   
+    // If you use this app with a front-end that needs id field instead of _id, you
+    //  have to override toJSON method that map default object to a custom object
+
     schema.method("toJSON", function() {
       const { __v, _id, ...object } = this.toObject();
       object.id = _id;
